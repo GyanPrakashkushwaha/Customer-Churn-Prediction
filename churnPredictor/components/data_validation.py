c7efe3843df087c8f7974580bc7edb60beeee77b
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-09-15 15:57:42 +0530

    add train and test data.

--- churnPredictor/components/data_validation.py
+++ churnPredictor/components/data_validation.py
@@ -1,15 +1,34 @@
 from entity import DataValidationConfig
+from sklearn.model_selection import train_test_split
 from churnPredictor import CustomException,logger
 import pandas as pd
+
+
 
 class DataValidataion:
     def __init__(self,config_in :DataValidationConfig):
         self.config_in = config_in
+
+    
+    def train_test_split_func(self):
+        df = pd.read_csv(self.config_in.data_dir)
+        train_df , test_df = train_test_split(df,test_size=0.20)
+
+        logger.info("Splited data into training and test sets")
+        logger.info(train_df.shape)
+        logger.info(test_df.shape)
+
+        train_df.to_csv(self.config_in.make_data.train_data,index=False)
+        test_df.to_csv(self.config_in.make_data.test_data,index=False)
+
+        return df
+
     
     def validate_features(self) -> bool:
         try:
             validation_status = False
-            df = pd.read_csv(self.config_in.data_dir)
+
+            df = self.train_test_split_func()
             df = df.drop(columns='Churn')
             all_features = list(df.columns)
             all_schema = self.config_in.schema_check
@@ -29,4 +48,5 @@ class DataValidataion:
             return validation_status
         except Exception as e:
             raise CustomException(e)
+        
         
