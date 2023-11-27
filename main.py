37c856a14fe11ccb861ca79396b764e76f796692
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-11-27 07:44:52 +0530

    removed redundent comment outs.

--- main.py
+++ main.py
@@ -6,53 +6,53 @@ from pipeline.stage_03_model_trainer import ModelTrainerPipeline
 from pipeline.stage_04_mflow_tracking import MlflowModelTracking 
 from pipeline.stage_05_mlflow_serving import MLFlowModelServing
 
-# STAGE_NAME = "Data Validation stage"
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_ingestion = DataValidationTrainingPipeline()
-#    data_ingestion.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#     logger.exception(e)
-#     raise CustomException(e)
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise CustomException(e)
 
 
-# STAGE_NAME = 'Data Transformation Stage'
+STAGE_NAME = 'Data Transformation Stage'
 
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_transformation = DataTransformationTrainingPipeline()
-#    data_transformation.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#    logger.exception(e)
-#    raise CustomException(e)
-
-
-
-# STAGE_NAME = "Model Training stage"
-
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    model_trainer = ModelTrainerPipeline()
-#    model_trainer.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#    logger.exception(e)
-#    raise CustomException(e)
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+   logger.exception(e)
+   raise CustomException(e)
 
 
 
-# STAGE_NAME = "Model Tracking Stage stage"
+STAGE_NAME = "Model Training stage"
 
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    mlflow_tracking = MlflowModelTracking()
-#    mlflow_tracking.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#    logger.exception(e)
-#    raise CustomException(e)
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   model_trainer = ModelTrainerPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+   logger.exception(e)
+   raise CustomException(e)
+
+
+
+STAGE_NAME = "Model Tracking Stage stage"
+
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   mlflow_tracking = MlflowModelTracking()
+   mlflow_tracking.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+   logger.exception(e)
+   raise CustomException(e)
 
 
 STAGE_NAME = "Model Serving stage"
